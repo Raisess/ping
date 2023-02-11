68f214c4d1d74dbe38310e6467f60ecd4907c7d0
Raisess <raisechif@gmail.com>
2023-02-11 15:52:39 -0300

    chore: things working

---
+++ src/main.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+import json
+from yacli import CLI, Command
+
+from service import Service, URL
+
+class CheckCommand(Command):
+  def __init__(self):
+    super().__init__("check", "", args_len=1)
+
+  def handle(self, args: list[str]) -> None:
+    path = args[0]
+    if not path.endswith(".json"):
+      raise Exception("Not a json file")
+
+    with open(path, "r") as json_file:
+      services_info: list[dict] = json.load(json_file)
+      for service_info in services_info:
+        url = URL(service_info.get("protocol"), service_info.get("host"), service_info.get("port"))
+        print(url.get_endpoint(""))
+        service = Service(service_info.get("name"), url)
+        print(f">>> Pinging: \033[33m{service.get_name()}\033[0m...")
+        for path in service_info.get("path_list") or []:
+          print(f"\t Route: {path}")
+          reponse = service.ping(path)
+          print(f"\t\t Elapsed time: {reponse.dureation()}ms")
+          if reponse.success():
+            print(f"\t\t Status: \033[32msuccess!\033[0m")
+          else:
+            print(f"\t\t Status: \033[31mfail!\033[0m")
+
+
+if __name__ == "__main__":
+  cli = CLI("ping", [CheckCommand()])
+  cli.handle()
---
+++ src/service.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from http.client import HTTPResponse
+from urllib import request
+
+class URL:
+  def __init__(self, protocol: str, host: str, port: int = None):
+    self.__protocol = protocol
+    self.__host = host
+    self.__port = port
+
+  def get_endpoint(self, path: str) -> str:
+    if self.__port:
+      return f"{self.__protocol}://{self.__host}:{self.__port}/{path}"
+
+    return f"{self.__protocol}://{self.__host}/{path}"
+
+
+class Response:
+  def __init__(self, code: int, start_time: int, end_time: int):
+    self.__code = code
+    self.__duration = end_time - start_time
+
+  def success(self) -> bool:
+    return self.__code == 200
+
+  def dureation(self) -> int:
+    return self.__duration
+
+
+class Service:
+  def __init__(self, name: str, url: URL):
+    self.__name = name
+    self.__url = url
+
+  def get_name(self) -> str:
+    return self.__name
+
+  def get_url(self) -> str:
+    return self.__url.get_endpoint("")
+
+  def ping(self, path: str = "") -> Response:
+    try:
+      start_time = int(datetime.now().timestamp())
+      response: HTTPResponse = request.urlopen(self.__url.get_endpoint(path))
+      return Response(response.getcode(), start_time, int(datetime.now().timestamp()))
+    except:
+      return Response(0, 0, 0)
